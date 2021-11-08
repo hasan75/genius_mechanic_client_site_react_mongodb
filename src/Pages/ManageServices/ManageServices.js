6e10ac2cccec33073cf3dbcd7d398542c04a6432
Hasan75 <hasanjab14@gmail.com>
2021-11-09 04:21:13 +0600

    Delete Operation functionalized

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -9,6 +9,23 @@ const ManageServices = () => {
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
+
+    const handleDelete = id => {
+        const url = `http://localhost:5000/services/${id}`
+        fetch(url,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount){
+                alert('SuccessFully Deleted the service')
+                const remainingServices = services.filter(service => service._id !== id);
+            setServices(remainingServices)
+            }
+            
+        })
+    }
+
     return (
         <div>
             <h2>Manage Services</h2>
@@ -16,7 +33,7 @@ const ManageServices = () => {
                 services.map(service => <div key={service._id} className="service-container-manage">
                     <h3>{service.name}</h3>
                     <h4>Price:{service.price}</h4>
-                    <button>Delete</button>
+                    <button onClick={()=> handleDelete(service._id)}>Delete</button>
                 </div>
                 )
             }
